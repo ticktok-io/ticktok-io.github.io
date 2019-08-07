db58a2c27fbcea2c46bdc0889ec367d68a864faf
Eli Segal <eli.segal@gmail.com>
2019-08-07 06:40:28 +0300

    circleci

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -8,7 +8,6 @@
 const React = require('react');
 
 const CompLibrary = require('../../core/CompLibrary.js');
-const Helmet = require('react-helmet');
 
 const MarkdownBlock = CompLibrary.MarkdownBlock; /* Used to read markdown */
 const Container = CompLibrary.Container;
