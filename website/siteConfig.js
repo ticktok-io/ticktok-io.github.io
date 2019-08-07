2ecdf295f3ea4599b85f790273a2b733137c7066
Eli Segal <eli.segal@gmail.com>
2019-08-07 07:09:02 +0300

    circleci

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,7 +23,7 @@ const users = [
 const siteConfig = {
   title: 'TICKTOK.IO', // Title for your website.
   tagline: 'Clock As a Service',
-  url: 'https://ticktok.io', // Your website URL
+  url: 'https://ticktok-io.github.io', // Your website URL
   baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
